cfcac8eeb594bd814c7c1694ecc744bf21e4aca8
Harold <haroldquekhere@gmail.com>
2023-05-23 18:31:54 +0800

    test

--- backend/settings.py
+++ backend/settings.py
@@ -130,8 +130,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 #     }
 # }
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+    'default': dj_database_url.parse(str(os.environ.get('DATABASE_URL')), conn_max_age=600),
 }
+
 # print(type(os.environ.get('DATABASE_URL')))
 
 
