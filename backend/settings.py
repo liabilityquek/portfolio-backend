1adbb22dbe569b92286fc58a43b72ce46b9a06c5
Harold <haroldquekhere@gmail.com>
2023-05-23 17:53:09 +0800

    added build.sh and amended settings.py for deployment

--- backend/settings.py
+++ backend/settings.py
@@ -16,6 +16,7 @@ from datetime import timedelta
 from django.conf import settings
 import mimetypes
 import os
+import dj_database_url
 
 load_dotenv()
 
@@ -33,9 +34,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
@@ -115,15 +116,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': "django.db.backends.postgresql",
-        'NAME': 'portfolio',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
     }
-}
+
 
 
 # Password validation
