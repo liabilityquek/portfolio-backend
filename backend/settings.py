d66d637d87b34d08450681bf82eed907de3c4a6b
Harold <haroldquekhere@gmail.com>
2023-05-23 18:04:24 +0800

    amended allowed_host in settings

--- backend/settings.py
+++ backend/settings.py
@@ -36,7 +36,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
+# Needed for production. Avoid using '*'.
+# amend when in production
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
