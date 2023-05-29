e2e6ae524cbe515fbfab748e66f2d9af0647c8c7
Harold <haroldquekhere@gmail.com>
2023-05-29 11:11:44 +0800

    install whitenoise and run collectstatic

--- backend/settings.py
+++ backend/settings.py
@@ -126,6 +126,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': dj_database_url.parse(str(os.environ.get('DATABASE_URL')), conn_max_age=600),
 }
@@ -165,6 +166,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
